e6e9b9b38bd5f276d1c7076d1c3b698ba09e6cbc
saiful-islam <saifulislam109710@gmail.com>
2024-07-06 20:03:22 +0600

    javascript file added

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+ScrollReveal({
+    distance: '150px',
+    origin: 'bottom',
+    delay:1000,
+}
+  
+)
+
+ScrollReveal().reveal('.subtitle',{
+    origin:"right",
+    delay:500,
+})
+
+ScrollReveal().reveal('.title',{
+    origin:"left",
+    delay:1000,
+})
+
+ScrollReveal().reveal('.card-container',{
+    origin:"bottom",
+    delay:200,
+})
+
+ScrollReveal().reveal('.btn',{
+    origin:"bottom",
+    delay:1200,
+})
